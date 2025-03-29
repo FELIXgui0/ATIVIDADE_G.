5722aa1828d9a152b21321f32b0351326d162486
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-29 19:02:31 -0300

    ex06.py

--- ex06.py
+++ ex06.py
@@ -1,13 +1,17 @@
-print("\n\nExercício 6: 100 primeiros números primos")
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 primos = []
 num = 2
 while len(primos) < 100:
-    primo = True
-    for i in range(2, num):
-        if num % i == 0:
-            primo = False
-            break
-    if primo:
+    if is_prime(num):
         primos.append(num)
     num += 1
+
 print(primos)
+
