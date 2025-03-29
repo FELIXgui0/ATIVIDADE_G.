f30e9fba4075cc0f84c786469e735a3f0b1f958d
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-29 19:07:00 -0300

    ex11.py

--- ex11.py
+++ ex11.py
@@ -1,7 +1,8 @@
-print("\nExercício 11: Sistema de login")
 usuario = input("Digite o nome de usuário: ")
 senha = input("Digite a senha: ")
+
 if usuario == senha:
-    print("Erro: Nome de usuário e senha não podem ser iguais.")
+    print("Nome de usuário e senha não podem ser iguais.")
 else:
-    print("Login permitido.")
+    print("Login realizado com sucesso.")
+
