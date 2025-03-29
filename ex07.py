f586523caa879775b01daa0a238a03a18f7fa089
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-29 19:03:24 -0300

    ex07.py

--- ex07.py
+++ ex07.py
@@ -1,9 +1,7 @@
-print("\nExercício 7: Diferença entre quadrados dos ímpares")
-numero = int(input("Digite um número ímpar: "))
-if numero % 2 == 0:
-    print("Número não é ímpar.")
-else:
-    anterior = (numero - 2)**2
-    proximo = (numero + 2)**2
-    diferenca = abs(anterior - proximo)
-    print("Diferença entre os quadrados:", diferenca)
+n = int(input("Digite um número ímpar: "))
+
+n_anterior = n - 2
+n_proximo = n + 2
+diferenca = (n_proximo ** 2) - (n_anterior ** 2)
+
+print(f"Diferença entre os quadrados: {diferenca}")
