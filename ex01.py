a783f428951e41a669d811135773f0265b30cd53
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-29 18:54:22 -0300

    ex01.py

--- ex01.py
+++ ex01.py
@@ -1,10 +1,9 @@
-print("Exercício 1: Cálculo de Médias")
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
-media_simples = (nota1 + nota2 + nota3) / 3
-media_ponderada_1 = (nota1*2 + nota2*2 + nota3*3) / 7
-media_ponderada_2 = (nota1*1 + nota2*2 + nota3*2) / 5
-print("Média simples:", media_simples)
-print("Média ponderada 1:", media_ponderada_1)
-print("Média ponderada 2:", media_ponderada_2)
+media_aritmetica = (nota1 + nota2 + nota3) / 3
+media_ponderada1 = (nota1 * 2 + nota2 * 2 + nota3 * 3) / (2 + 2 + 3)
+print(f"Média Ponderada (pesos 2, 2, 3): {media_ponderada1:.2f}")
+media_ponderada2 = (nota1 * 1 + nota2 * 2 + nota3 * 2) / (1 + 2 + 2)
+print(f"Média Ponderada (pesos 1, 2, 2): {media_ponderada2:.2f}")
+
