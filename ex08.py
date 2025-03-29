f0261ddde7acccc6a26efa83174a6bc857033066
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-29 19:04:27 -0300

    ex08.py

--- ex08.py
+++ ex08.py
@@ -1,4 +1,4 @@
-print("\nExercício 8: Converter Celsius para Fahrenheit")
 celsius = float(input("Digite a temperatura em Celsius: "))
-fahrenheit = celsius * 9/5 + 32
-print("Temperatura em Fahrenheit:", fahrenheit)
+
+fahrenheit = (celsius * 9/5) + 32
+print(f"A temperatura em Fahrenheit é: {fahrenheit:.2f}")
