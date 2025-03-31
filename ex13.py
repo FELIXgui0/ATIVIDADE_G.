093a69c5a622a914145a523fe15eb2196a7502bb
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-31 15:05:50 -0300

    Delete ex13.py

--- ex13.py
+++
@@ -1,7 +0,0 @@
-salario = float(input("Digite o salário inicial: "))
-anos = int(input("Digite o número de anos: "))
-
-for _ in range(anos):
-    salario *= 2
-
-print(f"Salário após {anos} anos: R${salario:.2f}")
