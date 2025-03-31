db10e37a709670577bb912b2952ca198a82e0a50
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-31 15:05:34 -0300

    Delete ex12.py

--- ex12.py
+++
@@ -1,8 +0,0 @@
-turmas = int(input("Digite a quantidade de turmas: "))
-alunos = int(input("Digite a quantidade total de alunos: "))
-
-media_alunos = alunos / turmas
-print(f"A média de alunos por turma é {media_alunos:.2f}")
-
-if media_alunos > 40:
-    print("Aviso: Algumas turmas têm mais de 40 alunos.")
