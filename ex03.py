73efb151f052df079ba11d6a2d38d17b4a424925
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-31 15:03:37 -0300

    Delete ex03.py

--- ex03.py
+++
@@ -1,8 +0,0 @@
-valor_total = float(input("Digite o valor total das mercadorias: "))
-
-if valor_total < 500:
-    imposto = 0
-else:
-    imposto = (valor_total - 500) * 0.5
-
-print(f"Imposto a ser pago: R${imposto:.2f}")
