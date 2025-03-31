fb02dbf057d23e3f1a00eca630ba57592b45b4ee
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-03-31 15:04:25 -0300

    Delete ex07.py

--- ex07.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Digite um número ímpar: "))
-
-n_anterior = n - 2
-n_proximo = n + 2
-diferenca = (n_proximo ** 2) - (n_anterior ** 2)
-
-print(f"Diferença entre os quadrados: {diferenca}")
