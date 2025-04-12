08f50a1faf28be379a084ef287957436eea055b1
FELIXgui0 <guilherme.rdfelix@gmail.com>
2025-04-12 15:29:12 -0300

    História Interativa

---
+++ Atividade_História_Interativa.py/Atv.História.Interativa.py
@@ -0,0 +1,9 @@
+cenario = input("Qual é o cenário da história? ")
+personagem = input("Quem é o personagem principal? ")
+coisa_encontrada = input("O que o personagem encontrou? ")
+onde_encontrou = input("Onde estava a coisa que ele encontrou? ")
+
+historia = f"{personagem} estava em {cenario} e encontrou um(a) {coisa_encontrada} no(a) {onde_encontrou}."
+
+print("\nA história gerada é:")
+print(historia)
